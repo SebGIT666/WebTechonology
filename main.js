1e1cdb38ce6759662316f94ca580b2142f9553da
SebGIT666 <0boros87@solent.ac.uk>
2026-03-22 12:41:09 +0000

    Added popup and portal CSS.Added JS event listeners for sound popup buttons

---
+++ main.js
@@ -0,0 +1,11 @@
+const popup = document.getElementById('popup');
+const btnSoundOn = document.getElementById('btn-sound-on');
+const btnSoundOff = document.getElementById('btn-sound-off');
+
+btnSoundOn.addEventListener('click', function() {
+    popup.style.display ='none';
+});
+
+btnSoundOff.addEventListener('click', function(){
+    popup.style.display= 'none';
+});
